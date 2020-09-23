f35526193d195900045eafca679dd9677cba9b78
Guillermo Croppi <guillermocroppi@gmail.com>
2020-09-23 16:15:42 -0300

    Bienvenido a Bienvenida!

--- ext/lib/notifier/translations/index.js
+++ ext/lib/notifier/translations/index.js
@@ -9,7 +9,7 @@ const overrides = {
   'templates.email.greeting': 'Hola {userName},',
   'templates.email.signature': `${config.organizationName} - ${config.bajadaPlataforma}`,
 
-  'templates.welcome-email.subject': 'Bienvenido a ' + config.organizationName,
+  'templates.welcome-email.subject': 'Bienvenida a ' + config.organizationName,
   'templates.welcome-email.body': `Iniciaste el proceso de registro para participar en <a href="${homeUrl}">${config.organizationName}</a>.<br><br>Para finalizarlo, hacé click <a href=\"{validateUrl}\">click aquí.</a>`,
   'templates.welcome-email.ps': 'En caso de no haberte registrado, por favor ignorá este correo.  ',
 
