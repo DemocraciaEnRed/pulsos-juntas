9bb08f500d511ea8bc97274a0684c6160cd1fff3
mijail cohen <mijaelcohen@gmail.com>
2021-01-21 15:15:46 -0300

    added changes

--- ext/lib/notifier/responsive-html-email-template.js
+++ ext/lib/notifier/responsive-html-email-template.js
@@ -18,6 +18,7 @@ module.exports = ({
     <head>
       <meta name="viewport" content="width=device-width" />
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+      
       <title>${config.organizationName}</title>
       <style>
         /* -------------------------------------
